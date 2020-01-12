d90a21c8e264d9734d9247a30a2d40e1776f88b5
Ecaterina Chirica <ecaterina.chirica14@gmail.com>
2020-01-12 13:30:21 +0000

    Backend

--- Express-Backend/app.js
+++ Express-Backend/app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/auth', auth);
 
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
